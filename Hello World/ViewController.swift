08b254d11dc3c1e6c38055ff615a1c584e2be70a
StevesRepository <sjdamico203@sbcglobal.net>
2016-01-27 21:01:11 -0600

    Week 2 Hello World
    Working with labels

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Steve D'Amico on 1/27/16.
+//  Copyright © 2016 Steve D'Amico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
